0cac810f3a6261a31d852bb9466a3e5ca174d54a
Dradwing <Rushbuket@gmail.com>
2022-03-05 08:20:07 +0530

    more files added

--- server/server.js
+++ server/server.js
@@ -1,1 +1,23 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const app = require("./app");
+const mongoose = require("mongoose");
 
+//connection to database
+const DB = process.env.DATABASE;
+const connectionParams = {
+  useNewUrlParser: true,
+};
+
+mongoose
+  .connect(DB, connectionParams)
+  .then((con) => {
+    console.log("Connected to database ");
+  })
+  .catch((err) => {
+    console.error(`Error connecting to the database. \n${err}`);
+  });
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running at port ${port}`);
+});
