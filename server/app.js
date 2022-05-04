f881ac8f6f6a264ff731baff3bb5a5ae85fddcda
Dradwing <Rushbuket@gmail.com>
2022-05-04 12:41:11 +0530

    changes2

--- server/app.js
+++ server/app.js
@@ -72,10 +72,15 @@ app.use("/contactUs", async (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
+
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
-} else app.use(express.static("../client/build"));
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 //global errors handler
 app.use(globalErrorHandler);
