2ac2e46e9e387599c8f78141923e766fc7a8421d
Dradwing <Rushbuket@gmail.com>
2022-05-05 22:58:25 +0530

    YSS Tester

--- client/src/components/maidDetailPage/bookingForm.jsx
+++ client/src/components/maidDetailPage/bookingForm.jsx
@@ -85,7 +85,7 @@ function BookingForm(props) {
                         <input
                           type="checkbox"
                           name="services"
-                          value="Cooking"
+                          value={service}
                           onClick={handleClickForInput}
                         />{" "}
                         {service}
